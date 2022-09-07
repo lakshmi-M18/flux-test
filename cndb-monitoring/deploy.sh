49dd60be1eee5a17f1b07660b5926cdc7bb30cb8
Lakshmi Manjunatha <89935678+lakshmi-M18@users.noreply.github.com>
2022-09-07 15:19:43 -0400

    Delete cndb-monitoring directory

--- cndb-monitoring/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# https://github.com/riptano/helm-chart-repository/ must be added as "datastax" repository
-helm upgrade cndb-monitoring datastax/cndb-monitoring --install --namespace monitor --history-max 5 --version "0.1.0+beb9ffc"  -f "./cndb-monitoring-dev-values.yaml"
